c886aae71a50e679bbc35ffb56e9ff33d020d1c0
Marc <marc@Marcs-MBP.attlocal.net>
2017-09-22 19:37:55 -0400

    Creating project skeleton

---
+++ src/main/java/com/marcharrison/resume/MarcResumeApplication.java
@@ -0,0 +1,12 @@
+package com.marcharrison.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcResumeApplication.class, args);
+	}
+}
